051e38b66d71ff32e8b5aabe3077c3093c2fbd45
Vaishnavi1506 <116016809+Vaishnavi1506@users.noreply.github.com>
2023-08-23 08:02:25 +0530

    index.js

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,4 +1,38 @@
-const hello=()=>{ 
+/*const hello=()=>{ 
 console.log("Hello GitHub!");
 };
-hello();
+hello();*/
+//write new file using fs
+/*const fs=require("fs");
+fs.writeFile(
+"sample.txt",
+"Hello World. Welcome to Node.js File System module.",
+(err)=>{
+if (err) throw err;
+console.log("File created");
+}
+);*/
+const fs = require("fs");
+//read file
+/*fs.readFile("sample.txt",(err, data)=> {
+if (err) throw err;
+console.log(data.toString());
+});
+*/
+//append file
+/*fs.appendFile("smaple.txt","this is my updated content",(err) => {
+if (err) throw err;
+console.log("File updated!");
+});
+*/
+//rename File
+/*fs.rename("sample.txt", "test.txt", (err) => {
+if (err) throw err;
+console.log("File name updated!");
+});
+*/
+//unlinkfile i.e delete files
+fs.unlink("test.txt",(err) => {
+  if(err) throw err;
+  console.log("File test.txt deletedsuccessfully!");
+});
