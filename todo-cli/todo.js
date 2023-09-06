18cf2533d368ffdea6b6cb186247e3c741dc3aed
Vaishnavi1506 <116016809+Vaishnavi1506@users.noreply.github.com>
2023-09-06 19:45:20 +0530

    level4

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -9,48 +9,27 @@ const todoList = () => {
   const overdue = () => {
     // Write the date check condition here and return the array
     // of overdue items accordingly.
-    overdues = []
-    for (let i = 0; i < all.length; i++) {
-      if (all[i].dueDate < new Date().toLocaleDateString('en-CA')) {
-        overdues.push(all[i])
-      }
-    }
-    return overdues
+    const today = new Date().toLocaleDateString("en-CA");
+    return all.filter((item) => item.dueDate < today);
   }
   const dueToday = () => {
-    // Write the date check condition here and return the array
-    // of todo items that are due today accordingly.
-    const today = new Date().toISOString().split("T")[0];
+    const today = new Date().toLocaleDateString("en-CA");
     return all.filter((item) => item.dueDate === today);
-  };
+  }
   const dueLater = () => {
-    // Write the date check condition here and return the array
-    // of todo items that are due later accordingly.
-    dueLaters = [];
-    for(let i =0; i< all.length; i++){
-      if(all[i].dueDate > new Date().toLocaleDateString("en-CA")){
-        dueLaters.push(all[i]);
-      }
-    }
-    return dueLaters;
-  };
-
+    const today = new Date().toLocaleDateString("en-CA");
+    return all.filter((item) => item.dueDate > today);
+  }
   const toDisplayableList = (list) => {
-    // Format the To-Do list here, and return the output string
-    // as per the format given above.
     return list
-    .map((item) => {
-      let checkbox = item.completed ? "[x]" : "[ ]";
-      const formattedDate =
-      item.dueDate !== new Date().toISOString().split("T")[0]
-      ? " " + item.dueDate
-      : "";
-      return [checkbox + " " + item.title + formattedDate];
-    })
-    .join("\n");
-   
-  };
-
+      .map(
+        (todo) =>
+          `${todo.completed ? "[x]" : "[ ]"} ${todo.title} ${
+            todo.dueDate == today ? "" : todo.dueDate
+          }`
+      )
+      .join("\n");
+  }
   return {
     all,
     add,
@@ -59,6 +38,6 @@ const todoList = () => {
     dueToday,
     dueLater,
     toDisplayableList
-  }
-}
-module.exports = todoList;
+  };
+};
+module.exports=todoList;
